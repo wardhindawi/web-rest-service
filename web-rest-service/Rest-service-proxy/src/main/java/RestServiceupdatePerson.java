16ef71def36a11435dc7c9fe02cf4c2f7daf6121
wardhindawi <ward.alhindawi@icsfs.com>
2021-12-01 14:15:31 +0200

    last modifications

---
+++ web-rest-service/Rest-service-proxy/src/main/java/RestServiceupdatePerson.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RestServiceupdatePerson {
+    public static void main(String[] args) {
+
+        try {
+
+            URL url = new URL("http://localhost:8080/RESTfulWebServicesSample/rest/persons/1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            String input = "{\"firstName\"dina\",\"lastName\":\"Khader\"}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
