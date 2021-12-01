aa77a8350f0e6bf5dd37afae0aaa4489990ee6b3
Dina Alkhader <Dina.Alkhader@icsfs.internal>
2021-12-01 12:31:25 +0200

    post modification

---
+++ web-rest-service/Rest-service-proxy/src/main/java/restServicePostPerson.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class restServicePostPerson {
+
+    public static void main(String[] args) {
+
+        try {
+
+            URL url = new URL("http://localhost:8080/RESTfulWebServicesSample/rest/persons/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            String input = "{\"id\":100,\"firstName\":'Dina'\"lastName\":'Khader'\"createdDate\":20/02/2020 }";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
